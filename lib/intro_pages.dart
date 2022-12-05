db7fb7d9cb068a9c095f8eaae9871e02e67bd65d
m-ardanmaulana19 <m.ardanmaulana19@gmail.com>
2022-12-06 02:11:19 +0700

    inisialisasi intro_pages

---
+++ lib/intro_pages.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroPages extends StatelessWidget {
+  const IntroPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+      //logo
+
+      //Penuhi kebutuhan si Kucing setiap hari
+
+      //Anda Pesan Kami Antarkan
+
+      //get started button
+    ]));
+  }
+}
